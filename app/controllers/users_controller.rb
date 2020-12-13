a70b810fff9b8ca1c88decff82a34fcedd7375c3
jesben <jesben.sevillano.h@uni.pe>
2020-12-12 19:16:36 -0500

    add bulma css and styled form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
 
     def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.create(params.require(:user).permit(:name, :email, :password))
     end
 
 end
+ 
